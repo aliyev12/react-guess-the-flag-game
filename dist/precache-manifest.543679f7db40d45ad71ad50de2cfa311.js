a53b8fc07b84267adafd6393547ffb8cfc5841d8
aliyev12 <aaa719@nyu.edu>
2019-01-23 15:15:36 -0500

    install gh-pages and built dist folder ready to deploy to github pages

---
+++ dist/precache-manifest.543679f7db40d45ad71ad50de2cfa311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f013425455756498f93f9c6fa6b4458",
+    "url": "/react-guess-the-flag-game/static/media/earth.1f013425.jpg"
+  },
+  {
+    "revision": "1c83624f7a117d1d837f",
+    "url": "/react-guess-the-flag-game/static/js/runtime~main.1c83624f.js"
+  },
+  {
+    "revision": "349003944a7cded25b3d",
+    "url": "/react-guess-the-flag-game/static/js/main.34900394.chunk.js"
+  },
+  {
+    "revision": "df6c8c13be8b019ca91a",
+    "url": "/react-guess-the-flag-game/static/js/1.df6c8c13.chunk.js"
+  },
+  {
+    "revision": "349003944a7cded25b3d",
+    "url": "/react-guess-the-flag-game/static/css/main.cab2c48b.chunk.css"
+  },
+  {
+    "revision": "4527434a6d4581d95f161c099cb0193f",
+    "url": "/react-guess-the-flag-game/index.html"
+  }
+];
